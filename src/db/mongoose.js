f6dda12f34f38f702416a3bc7480ed60afb8bff7
Rafael Pelle <rafael.pelle@mercadolivre.com>
2023-12-05 23:18:28 -0300

    feat: mongoose connection feedbacks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,6 +5,18 @@ dotenv.config();
 
 const { DATABASE_URI, DATABASE_NAME } = process.env;
 
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connection open to ' + DATABASE_NAME);
+});
+
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose connection disconnected');
+});
+
 mongoose.connect(`${DATABASE_URI}/${DATABASE_NAME}?retryWrites=true`, {
   useNewUrlParser: true,
 });
